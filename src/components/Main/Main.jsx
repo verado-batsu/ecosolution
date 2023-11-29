3c348f0b562d92bf0da3f0e55876692144a18c62
Vladyslav <kupchuk.vlad@gmail.com>
2023-11-29 19:32:00 +0200

    FINISHED: main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,12 +6,16 @@ import { findHeaderHeight } from 'helpers/findHeaderHeight';
 import { ReactComponent as ArrowIcon } from 'assets/images/main/arrow-right.svg';
 import windTurbine from 'assets/images/main/windTurbine.jpg';
 import windTurbine2x from 'assets/images/main/windTurbine@2x.jpg';
+import windTurbineDesktop from 'assets/images/main/windTurbineDesktop.jpg';
+import windTurbineDesktop2x from 'assets/images/main/windTurbineDesktop@2x.jpg';
 
 import styles from './Main.module.scss';
 const {
     main,
+    mainContainer,
     mainTopWrapper,
     mainTitle,
+    preTitleWrapper,
     mainPreTitle,
     mainBtnWrapper,
     learnMoreBtn,
@@ -38,27 +42,29 @@ export function Main() {
 
     return (
         <section name="Main" className={main}>
-            <div className="container">
+            <div className={`container ${mainContainer}`}>
                 <div className={mainTopWrapper}>
                     <h1 className={mainTitle}>RENEWABLE ENERGY For any task</h1>
-                    <p className={mainPreTitle}>
-                        Development and implementation of renewable
-                        non-polluting energy sources, generating power
-                        generation using energy wind, sun, water, biomass
-                    </p>
-                    <div className={mainBtnWrapper}>
-                        <Link
-                            to="Cases"
-                            className={learnMoreBtn}
-                            smooth={true}
-                            offset={Number(`-${headerHeight}`)}
-                            duration={500}
-                        >
-                            <span>Learn more</span>
-                            <div className={mainArrowWrapper}>
-                                <ArrowIcon className={mainArrowIcon} />
-                            </div>
-                        </Link>
+                    <div className={preTitleWrapper}>
+                        <p className={mainPreTitle}>
+                            Development and implementation of renewable
+                            non-polluting energy sources, generating power
+                            generation using energy wind, sun, water, biomass
+                        </p>
+                        <div className={mainBtnWrapper}>
+                            <Link
+                                to="Cases"
+                                className={learnMoreBtn}
+                                smooth={true}
+                                offset={Number(`-${headerHeight}`)}
+                                duration={500}
+                            >
+                                <span>Learn more</span>
+                                <div className={mainArrowWrapper}>
+                                    <ArrowIcon className={mainArrowIcon} />
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
                 <div className={mainBottomWrapper}>
@@ -73,12 +79,21 @@ export function Main() {
                             ecosolution &copy; 2023
                         </span>
                     </p>
-                    <img
-                        className={windTurbineImg}
-                        srcSet={`${windTurbine} 1x, ${windTurbine2x} 2x`}
-                        src={windTurbine}
-                        alt="wind turbine clean energy"
-                    />
+                    {isDesktop ? (
+                        <img
+                            className={windTurbineImg}
+                            srcSet={`${windTurbineDesktop} 1x, ${windTurbineDesktop2x} 2x`}
+                            src={windTurbineDesktop}
+                            alt="wind turbine clean energy"
+                        />
+                    ) : (
+                        <img
+                            className={windTurbineImg}
+                            srcSet={`${windTurbine} 1x, ${windTurbine2x} 2x`}
+                            src={windTurbine}
+                            alt="wind turbine clean energy"
+                        />
+                    )}
                 </div>
             </div>
         </section>
