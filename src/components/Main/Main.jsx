88cc12b1ed13e2b6a5b8cc6cdf7d7bc87560b36e
Vladyslav <kupchuk.vlad@gmail.com>
2023-11-29 15:49:45 +0200

    ADDED: title and pre title of main section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,32 +1,15 @@
 import styles from './Main.module.scss';
-const { main } = styles;
+const { main, mainTitle, mainPreTitle } = styles;
 
 export function Main() {
     return (
         <section name="Main" className={main}>
             <div className="container">
-                <h1>MAIN</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Quasi, molestiae quam ipsa illum, minima error voluptatum
-                    reprehenderit nisi labore eum et dolore voluptate, odio quod
-                    velit possimus sequi nobis vel! Suscipit cumque, sapiente
-                    iusto id mollitia perferendis dolore minima voluptate quia
-                    voluptatum officiis praesentium totam, voluptas, libero rem?
-                    Possimus amet quibusdam, doloribus quis esse labore
-                    distinctio ea non nihil rem. Quidem, omnis voluptas? Vitae,
-                    dolorem quasi numquam, quidem magni praesentium cupiditate
-                    perferendis deserunt laudantium adipisci quo ipsam velit
-                    doloremque omnis fuga? Delectus, repudiandae temporibus
-                    vitae at dolorum molestiae cum quasi. Quam dolorem in iure
-                    iusto corrupti voluptatibus, excepturi explicabo placeat ut
-                    nisi obcaecati libero. Facilis incidunt, quas tenetur
-                    perspiciatis dolorem, natus beatae molestiae facere dolor
-                    labore odio provident voluptate cupiditate! Iusto magnam
-                    itaque numquam in eum perspiciatis, quisquam sit pariatur.
-                    Eos facere nulla a ut ab mollitia rerum laborum tempora,
-                    vitae tenetur omnis atque fugiat laboriosam incidunt
-                    inventore nobis obcaecati?
+                <h1 className={mainTitle}>RENEWABLE ENERGY For any task</h1>
+                <p className={mainPreTitle}>
+                    Development and implementation of renewable non-polluting
+                    energy sources, generating power generation using energy
+                    wind, sun, water, biomass
                 </p>
             </div>
         </section>
