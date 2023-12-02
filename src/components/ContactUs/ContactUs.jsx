563280babd4b289822b921b1e7ea8367ff07f880
Vladyslav <kupchuk.vlad@gmail.com>
2023-12-02 03:47:24 +0200

    FINISHED: contact us section

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -12,6 +12,7 @@ import styles from './ContactUs.module.scss';
 const {
     contactUs,
     contactUsTitle,
+    contactUsContentWrapper,
     contactSocialList,
     contactSocialItem,
     contactSocialTitle,
@@ -27,73 +28,92 @@ export function ContactUs() {
         <section name="Contact Us" className={contactUs}>
             <div className="container">
                 <h2 className={contactUsTitle}>Contact us</h2>
-                <ul className={contactSocialList}>
-                    <li className={contactSocialItem}>
-                        <h3 className={contactSocialTitle}>Phone:</h3>
-                        <div>
-                            <a className={contactLink} href="tel:+380981234567">
-                                <div className={iconWrapper}>
-                                    <PhoneIcon className={contactSocialIcon} />
-                                </div>
-                                <span>38 (098) 12 34 567</span>
-                            </a>
-                            <a className={contactLink} href="tel:+380981234567">
-                                <div className={iconWrapper}>
-                                    <PhoneIcon className={contactSocialIcon} />
-                                </div>
-                                <span>38 (093) 12 34 567</span>
-                            </a>
-                        </div>
-                    </li>
-                    <li className={contactSocialItem}>
-                        <h3 className={contactSocialTitle}>E-mail:</h3>
-                        <a
-                            className={contactLink}
-                            href="mailto:office@ecosolution.com"
-                        >
-                            <div className={iconWrapper}>
-                                <SmsIcon className={contactSocialIcon} />
+                <div className={contactUsContentWrapper}>
+                    <ul className={contactSocialList}>
+                        <li className={contactSocialItem}>
+                            <h3 className={contactSocialTitle}>Phone:</h3>
+                            <div>
+                                <a
+                                    className={contactLink}
+                                    href="tel:+380981234567"
+                                >
+                                    <div className={iconWrapper}>
+                                        <PhoneIcon
+                                            className={contactSocialIcon}
+                                        />
+                                    </div>
+                                    <span>38 (098) 12 34 567</span>
+                                </a>
+                                <a
+                                    className={contactLink}
+                                    href="tel:+380981234567"
+                                >
+                                    <div className={iconWrapper}>
+                                        <PhoneIcon
+                                            className={contactSocialIcon}
+                                        />
+                                    </div>
+                                    <span>38 (093) 12 34 567</span>
+                                </a>
                             </div>
-                            <span>office@ecosolution.com</span>
-                        </a>
-                    </li>
-                    <li className={contactSocialItem}>
-                        <h3 className={contactSocialTitle}>Address:</h3>
-                        <a
-                            className={contactLink}
-                            href="https://maps.app.goo.gl/4wEMGGNfCrechPkS6"
-                            target="_blank"
-                            rel="noopener noreferrer nofollow"
-                        >
-                            <div className={iconWrapper}>
-                                <MapIcon className={contactSocialIcon} />
-                            </div>
-                            <span>
-                                79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
-                            </span>
-                        </a>
-                    </li>
-                    <li className={contactSocialItem}>
-                        <h3 className={contactSocialTitle}>Social Networks:</h3>
-                        <div className={contactSocialLinkWrapper}>
+                        </li>
+                        <li className={contactSocialItem}>
+                            <h3 className={contactSocialTitle}>E-mail:</h3>
                             <a
-                                className={contactSocialLink}
-                                href="."
+                                className={contactLink}
+                                href="mailto:office@ecosolution.com"
+                            >
+                                <div className={iconWrapper}>
+                                    <SmsIcon className={contactSocialIcon} />
+                                </div>
+                                <span>office@ecosolution.com</span>
+                            </a>
+                        </li>
+                        <li className={contactSocialItem}>
+                            <h3 className={contactSocialTitle}>Address:</h3>
+                            <a
+                                className={contactLink}
+                                href="https://maps.app.goo.gl/4wEMGGNfCrechPkS6"
+                                target="_blank"
                                 rel="noopener noreferrer nofollow"
                             >
-                                <FacebookIcon className={contactSocialIcon} />
+                                <div className={iconWrapper}>
+                                    <MapIcon className={contactSocialIcon} />
+                                </div>
+                                <span>
+                                    79005, Ukraine, Lvivstreet. Shota Rustaveli,
+                                    7
+                                </span>
                             </a>
-                            <a
-                                className={contactSocialLink}
-                                href="."
-                                rel="noopener noreferrer nofollow"
-                            >
-                                <InstagramIcon className={contactSocialIcon} />
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-                <ContactUsForm />
+                        </li>
+                        <li className={contactSocialItem}>
+                            <h3 className={contactSocialTitle}>
+                                Social Networks:
+                            </h3>
+                            <div className={contactSocialLinkWrapper}>
+                                <a
+                                    className={contactSocialLink}
+                                    href="."
+                                    rel="noopener noreferrer nofollow"
+                                >
+                                    <FacebookIcon
+                                        className={contactSocialIcon}
+                                    />
+                                </a>
+                                <a
+                                    className={contactSocialLink}
+                                    href="."
+                                    rel="noopener noreferrer nofollow"
+                                >
+                                    <InstagramIcon
+                                        className={contactSocialIcon}
+                                    />
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ContactUsForm />
+                </div>
             </div>
         </section>
     );
