8856bd65f2740ef27e05c5bc365913b50833cf74
Vladyslav <kupchuk.vlad@gmail.com>
2023-12-02 05:04:00 +0200

    ADDED: mobile and tablet versions of footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,146 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-scroll';
+import { useMediaQuery } from 'react-responsive';
+
+import { findHeaderHeight } from 'helpers/findHeaderHeight';
+
+import footerLogo from 'assets/images/footer/logo.png';
+import {
+    ArrowUpIcon,
+    FacebookIcon,
+    InstagramIcon,
+} from 'assets/images/footer/icons';
+
+import styles from './Footer.module.scss';
+const {
+    footer,
+    footerBody,
+    footerTop,
+    footerLogoLink,
+    logo,
+    footerUp,
+    footerArrowUpIcon,
+    footerSocialList,
+    footerSocialItem,
+    footerSocialLink,
+    footerSocialIcon,
+    footerBottom,
+    footerLinkList,
+    footerLinkItem,
+    footerLink,
+    footerCopy,
+} = styles;
+
 export function Footer() {
+    const [headerHeight, setHeaderHeight] = useState(() => findHeaderHeight());
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+    const isDesktop = useMediaQuery({ minWidth: 1440 });
+
+    useEffect(() => {
+        setHeaderHeight(findHeaderHeight());
+    }, [headerHeight, isMobile, isTablet, isDesktop]);
+
     return (
-        <section>
+        <section className={footer}>
             <div className="container">
-                <h1>FOOTER</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Quasi, molestiae quam ipsa illum, minima error voluptatum
-                    reprehenderit nisi labore eum et dolore voluptate, odio quod
-                    velit possimus sequi nobis vel! Suscipit cumque, sapiente
-                    iusto id mollitia perferendis dolore minima voluptate quia
-                    voluptatum officiis praesentium totam, voluptas, libero rem?
-                    Possimus amet quibusdam, doloribus quis esse labore
-                    distinctio ea non nihil rem. Quidem, omnis voluptas? Vitae,
-                    dolorem quasi numquam, quidem magni praesentium cupiditate
-                    perferendis deserunt laudantium adipisci quo ipsam velit
-                    doloremque omnis fuga? Delectus, repudiandae temporibus
-                    vitae at dolorum molestiae cum quasi. Quam dolorem in iure
-                    iusto corrupti voluptatibus, excepturi explicabo placeat ut
-                    nisi obcaecati libero. Facilis incidunt, quas tenetur
-                    perspiciatis dolorem, natus beatae molestiae facere dolor
-                    labore odio provident voluptate cupiditate! Iusto magnam
-                    itaque numquam in eum perspiciatis, quisquam sit pariatur.
-                    Eos facere nulla a ut ab mollitia rerum laborum tempora,
-                    vitae tenetur omnis atque fugiat laboriosam incidunt
-                    inventore nobis obcaecati?
-                </p>
+                <div className={footerBody}>
+                    <div className={footerTop}>
+                        <a className={footerLogoLink} href="./">
+                            <img className={logo} src={footerLogo} alt="logo" />
+                        </a>
+                        {isTablet || isDesktop ? (
+                            <ul className={footerSocialList}>
+                                <li className={footerSocialItem}>
+                                    <a
+                                        className={footerSocialLink}
+                                        href="."
+                                        rel="noopener noreferrer nofollow"
+                                    >
+                                        <FacebookIcon
+                                            className={footerSocialIcon}
+                                        />
+                                    </a>
+                                </li>
+                                <li className={footerSocialItem}>
+                                    <a
+                                        className={footerSocialLink}
+                                        href="."
+                                        rel="noopener noreferrer nofollow"
+                                    >
+                                        <InstagramIcon
+                                            className={footerSocialIcon}
+                                        />
+                                    </a>
+                                </li>
+                            </ul>
+                        ) : (
+                            ''
+                        )}
+                        <Link
+                            to="Main"
+                            className={footerUp}
+                            smooth={true}
+                            offset={Number(`-${headerHeight}`)}
+                            duration={500}
+                        >
+                            <ArrowUpIcon className={footerArrowUpIcon} />
+                        </Link>
+                    </div>
+                    {isMobile && (
+                        <ul className={footerSocialList}>
+                            <li className={footerSocialItem}>
+                                <a
+                                    className={footerSocialLink}
+                                    href="."
+                                    rel="noopener noreferrer nofollow"
+                                >
+                                    <FacebookIcon
+                                        className={footerSocialIcon}
+                                    />
+                                </a>
+                            </li>
+                            <li className={footerSocialItem}>
+                                <a
+                                    className={footerSocialLink}
+                                    href="."
+                                    rel="noopener noreferrer nofollow"
+                                >
+                                    <InstagramIcon
+                                        className={footerSocialIcon}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    )}
+
+                    <ul className={footerLinkList}>
+                        <li className={footerLinkItem}>
+                            <a
+                                className={footerLink}
+                                href="https://maps.app.goo.gl/4wEMGGNfCrechPkS6"
+                                target="_blank"
+                                rel="noopener noreferrer nofollow"
+                            >
+                                79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
+                            </a>
+                        </li>
+                        <li className={footerLinkItem}>
+                            <a
+                                className={footerLink}
+                                href="mailto:office@ecosolution.com"
+                            >
+                                office@ecosolution.com
+                            </a>
+                        </li>
+                        <li className={footerLinkItem}>
+                            <p className={footerCopy}>
+                                ecosolution &copy; 2023
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     );
