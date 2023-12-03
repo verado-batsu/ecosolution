ca3a795bb939ddf92e0f6272c5fad502a7dbe4f7
Vladyslav <kupchuk.vlad@gmail.com>
2023-12-03 17:11:36 +0200

    DELETED: unnecessary function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { useEffect, useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
-
-import { findHeaderHeight } from 'helpers/findHeaderHeight';
-
 import './App.scss';
 
 import { Main } from 'components/Main/Main';
@@ -15,16 +10,6 @@ import { ContactUs } from 'components/ContactUs/ContactUs';
 import { Footer } from 'components/Footer/Footer';
 
 function App() {
-    const [headerHeight, setHeaderHeight] = useState(() => findHeaderHeight());
-
-    const isMobile = useMediaQuery({ maxWidth: 767 });
-    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
-    const isDesktop = useMediaQuery({ minWidth: 1440 });
-
-    useEffect(() => {
-        setHeaderHeight(findHeaderHeight());
-    }, [headerHeight, isMobile, isTablet, isDesktop]);
-
     return (
         <>
             <Header />
